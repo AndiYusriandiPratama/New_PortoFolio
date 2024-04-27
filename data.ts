7cbc79de40975474fb02f1b94cdef6094b4400ea
AndiYusriandiPratama <andiyusriandiprtm@gmail.com>
2024-04-27 16:21:34 +0800

    update data.ts

--- data.ts
+++ data.ts
@@ -1,11 +1,15 @@
 import { IExperience, IProject, IService, ISkill } from "./type";
-import {
-  GrPersonalComputer,
-  GrPaint,
-  GrDatabase,
-  GrSystem,
-  GrBusinessService,
-} from "react-icons/gr";
+// import {
+//   GrPersonalComputer,
+//   GrPaint,
+//   GrDatabase,
+//   GrSystem,
+//   GrBusinessService,
+// } from "react-icons/gr";
+import { RiComputerLine } from "react-icons/ri";
+import { FaServer } from "react-icons/fa";
+import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
+import { MdDeveloperMode } from "react-icons/md";
 import UpworkLogo from "./public/logo_upwork.webp";
 import PurwadhikaLogo from "./public/logo_purwadhika.webp";
 import DicodingLogo from "./public/logo_dicoding.webp";
@@ -31,32 +35,31 @@ import DandelionImage from "./public/Project_image/Dandelion.png";
 
 export const services: IService[] = [
   {
-    Icon: GrPersonalComputer,
+    Icon: RiComputerLine,
     title: "Front-End",
     about:
       "I Use <b>NextJs</b>, <b>Tailwind</b>, and <b>State Management</b> for make a Project.",
   },
   {
-    Icon: GrDatabase,
-
+    Icon: FaServer,
     title: "Back-End",
     about:
       "I Use <b>ExpressJs</b>, <b>Mysql</b>, <b>MongoDb</b>, <b>Firebase</b>, <b>Sequelize</b>, and <b>Midtrans</b>.",
   },
   {
-    Icon: GrSystem,
+    Icon: MdDeveloperMode,
     title: "Machine Learning",
     about:
       "I understand a little about machine learning, especially <b>SVM</b> and <b>K-NN</b>.",
   },
   {
-    Icon: GrPaint,
+    Icon: AiOutlineAntDesign,
     title: "Designer",
     about:
       "Skill Design with <b>Photoshop</b>, <b>Corel Draw</b>, and <b>Figma</b>.",
   },
   {
-    Icon: GrBusinessService,
+    Icon: AiOutlineApi,
     title: "Other",
     about:
       "I work with data processing such as <b>Excel</b>, <b>Word</b>, and <b>Python</b>.",
